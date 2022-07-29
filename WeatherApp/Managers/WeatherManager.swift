67fdd914a668be093d3589a5ce1e732338920afc
Andrew Le <s3820098@rmit.edu.vn>
2022-07-29 20:32:04 +0700

    5. create WeatherManager and WeatherResponseBody

---
+++ WeatherApp/Managers/WeatherManager.swift
@@ -0,0 +1,37 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by Andrew Le Nguyen on 29/07/2022.
+//
+
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+    /*
+        Method to make HTTP request to get weather from OpenWeather API
+    */
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> WeatherResponseModel {
+        // API key
+        let API_KEY = "8cefcd7047392abd796820e4610483a9";
+        
+        // create URL with suitable queries
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(API_KEY)&units=metric") else { fatalError("Missing URL") }
+        
+        // create URL request
+        let urlRequest = URLRequest(url: url)
+        
+        // create fetching session
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        // if the request made successffully
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching weather data") }
+        
+        // decode JSON response and return
+        let decodedData = try JSONDecoder().decode(WeatherResponseModel.self, from: data)
+        
+        return decodedData
+    }
+}
