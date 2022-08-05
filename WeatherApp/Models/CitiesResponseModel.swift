a6f5cffa2d3ed65d8d152f611f06c02f772ba5a2
Andrew Le <s3820098@rmit.edu.vn>
2022-08-05 16:52:45 +0700

    22. add asm header

--- WeatherApp/Models/CitiesResponseModel.swift
+++ WeatherApp/Models/CitiesResponseModel.swift
@@ -1,3 +1,20 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Le Nguyen Truong An
+  ID: 3820098
+  Last modified: 05/08/2022
+  Acknowledgement:
+    + https://www.hackingwithswift.com/
+    + https://openweathermap.org/
+    + https://stackoverflow.com/
+    + https://peterfriese.dev/
+    + https://www.tutorialspoint.com/
+    + https://www.github.com/
+*/
+
 //
 //  CitiesResponseModel.swift
 //  WeatherApp
